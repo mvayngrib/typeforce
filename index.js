94307fe9218eb5c14c6d7a7aad51112c80be8d59
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-12 09:38:22 -0400

    support type|otherType notation

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ function getName (value) {
   return match ? match[1] : null
 }
 
-module.exports = function enforce (type, value) {
+var byType = {
+  string: stringCheck,
+  object: objCheck
+}
+
+var enforce = module.exports = function (type, value) {
   if (typeof type === 'string') {
     if (type[0] === '?') {
       if (value === null || value === undefined) {
@@ -22,81 +27,84 @@ module.exports = function enforce (type, value) {
     }
   }
 
-  switch (type) {
-    case 'Array': {
-      if (value !== null && value !== undefined && value.constructor === Array) return
-      break
-    }
-
-    case 'Boolean': {
-      if (typeof value === 'boolean') return
-      break
-    }
-
-    case 'Buffer': {
-      if (Buffer.isBuffer(value)) return
-      break
-    }
-
-    case 'Function': {
-      if (typeof value === 'function') return
-      break
-    }
-
-    case 'Number': {
-      if (typeof value === 'number') return
-      break
-    }
-
-    case 'Object': {
-      if (typeof value === 'object') return
-      break
-    }
-
-    case 'String': {
-      if (typeof value === 'string') return
-      break
-    }
-
-    default: {
-      switch (typeof type) {
-        case 'string': {
-          if (type === getName(value)) return
-          break
-        }
-
-        // evaluate type templates
-        case 'object': {
-          if (Array.isArray(type)) {
-            var subType = type[0]
-
-            enforce('Array', value)
-            value.forEach(enforce.bind(undefined, subType))
-
-            return
-          }
-
-          enforce('Object', value)
-          for (var propertyName in type) {
-            var propertyType = type[propertyName]
-            var propertyValue = value[propertyName]
-
-            try {
-              enforce(propertyType, propertyValue)
-            } catch (e) {
-              throw new TypeError('Expected property "' + propertyName + '" of type ' + JSON.stringify(propertyType) + ', got ' + getName(propertyValue) + ' ' + propertyValue)
-            }
-          }
-
-          return
-        }
-      }
-    }
+  if ((typeof type) in byType) {
+    byType[typeof type](type, value)
+  } else {
+    throwDefault(type, value)
   }
-
-  throwDefault(type, value)
 }
 
-function throwDefault(type, value) {
+function stringCheck (type, value) {
+  var passed = type.split('|').some(function (subType) {
+    switch (subType) {
+      case 'Array': {
+        if (value !== null && value !== undefined && value.constructor === Array) {
+          return true
+        }
+
+        break
+      }
+
+      case 'Boolean': {
+        if (typeof value === 'boolean') return true
+        break
+      }
+
+      case 'Buffer': {
+        if (Buffer.isBuffer(value)) return true
+        break
+      }
+
+      case 'Function': {
+        if (typeof value === 'function') return true
+        break
+      }
+
+      case 'Number': {
+        if (typeof value === 'number') return true
+        break
+      }
+
+      case 'Object': {
+        if (typeof value === 'object') return true
+        break
+      }
+
+      case 'String': {
+        if (typeof value === 'string') return true
+        break
+      }
+    }
+
+    if (getName(value) === subType) return true
+  })
+
+  if (!passed) throwDefault(type, value)
+}
+
+function objCheck (type, value) {
+  if (Array.isArray(type)) {
+    var subType = type[0]
+
+    enforce('Array', value)
+    value.forEach(enforce.bind(undefined, subType))
+
+    return
+  }
+
+  enforce('Object', value)
+  for (var propertyName in type) {
+    var propertyType = type[propertyName]
+    var propertyValue = value[propertyName]
+
+    try {
+      enforce(propertyType, propertyValue)
+    } catch (e) {
+      throw new TypeError('Expected property "' + propertyName + '" of type ' + JSON.stringify(propertyType) + ', got ' + getName(propertyValue) + ' ' + propertyValue)
+    }
+  }
+}
+
+function throwDefault (type, value) {
   throw new TypeError('Expected ' + type + ', got ' + getName(value) + ' ' + value)
 }
